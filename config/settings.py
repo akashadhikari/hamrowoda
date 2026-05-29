3e7284ea221cad70f18ba2576d8840a89cbb4c5e
Akash Adhikari <akashadhikari@Akashs-MacBook-Pro-2.local>
2026-05-29 15:56:10 +0545

    Remove pagination settings for DRF

--- config/settings.py
+++ config/settings.py
@@ -78,13 +78,6 @@ DATABASES = {
 }
 
 
-# Django REST Framework
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 200,
-}
-
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
